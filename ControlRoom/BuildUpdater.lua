b88cee792f77ead154be308dd9a7cd5748b853fe
Daniel Kňourek <dev.danielknourek@gmail.com>
2019-03-27 01:49:56 +0100

    BuildUpdater

--- ControlRoom/BuildUpdater.lua
+++ ControlRoom/BuildUpdater.lua
@@ -1,8 +1,21 @@
-print("Hello world !")
-local file_GUI = http://danielknourek.9e.cz/.CCcode/ControlRoom/GUI.lua	
+local file_GUI = "http://danielknourek.9e.cz/.CCcode/ControlRoom/GUI.lua"
+local file_name = "GUI.lua"
 while true do
     event, side, xPos, yPos = os.pullEvent("monitor_touch")
-    print(event .. " => Side: " .. tostring(side) .. ", " ..
-      "X: " .. tostring(xPos) .. ", " ..
-      "Y: " .. tostring(yPos))
+
+    if (xPos == 1 and yPos==1) then
+        break
+    end
+
+    shell.run("delete",file_name)
+    shell.run("wget","http://danielknourek.9e.cz/.CCcode/ControlRoom/GUI.lua")
+    shell.run("delete","disk/ControlRoom/GUI.lua")
+    shell.run("copy","GUI.lua","disk/ControlRoom/GUI.lua")
+
+    
+    print(os.time())
+    print("Updated!")
+
+
   end
+  
